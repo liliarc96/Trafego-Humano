acfce19a9fddb64379bddedd7e9f6ffbb93d112b
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-06-13 00:50:09 -0300

    Update DetectorDeFace.h

--- TrafegoDePessoas/include/DetectorDeFace.h
+++ TrafegoDePessoas/include/DetectorDeFace.h
@@ -1,6 +1,11 @@
 #ifndef DETECTORDEFACE_H
 #define DETECTORDEFACE_H
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
+#include <iostream>
+#include <stdio.h>
 
 class DetectorDeFace
 {
